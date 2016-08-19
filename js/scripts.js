5bdc82823c907018bb55eddc173b3bdbd6d40bb5
Sara Jensen <sara.jensen@idealist.org>
2016-08-19 08:56:18 -0700

    include content in html, including about the author section, instructions to play the game, the input form and submit button. Began basic JS to log user number input value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+// Begin Business Logic
 
+
+
+// Begin User Interface Logic
+$(document).ready(function() {
+  $("form#userInputForm").submit(function(event) {
+    event.preventDefault();
+
+    var numberInput = $("#userNumberInput").val();
+    console.log(numberInput);
+
+  });
 });
