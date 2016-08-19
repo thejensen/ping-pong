11c662ed7511e6e2a638ad177390b7bc8900d84d
Sara Jensen <sara.jensen@idealist.org>
2016-08-19 10:48:27 -0700

    finish business logic that shows ping as long as the number is divisible by 3 in a list of numbers, shows pong if the number is divisible by 5 and pingpong if the number is divisible by 3 and 5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,48 @@
 // Begin Business Logic
 
-
-
 // Begin User Interface Logic
 $(document).ready(function() {
   $("form#userInputForm").submit(function(event) {
     event.preventDefault();
 
-    var numberInput = $("#userNumberInput").val();
-    console.log(numberInput);
+    var number = parseInt($("#userNumberInput").val());
+    var arrayNumbers = [];
+
+
+    for (var i = 0; i <= number - 1; i++) {
+      arrayNumbers.push(i + 1);
+      console.log(arrayNumbers);
+      if (arrayNumbers[i] % 3 === 0 && arrayNumbers[i] % 5 === 0) {
+        arrayNumbers.pop();
+        arrayNumbers.push("pingpong");
+      }
+      if (arrayNumbers[i] % 3 === 0) {
+        arrayNumbers.pop();
+        arrayNumbers.push("ping");
+      }
+      if (arrayNumbers[i] % 5 === 0) {
+        arrayNumbers.pop();
+        arrayNumbers.push("pong");
+      }
+      $("#output").append("<li>" + arrayNumbers[i] + "</li>");
+    };
+
+
+
 
   });
 });
+
+
+// Satisfies criteria 1-2 and 8a (numbers in a list...)
+// $(document).ready(function() {
+//   $("form#userInputForm").submit(function(event) {
+//     event.preventDefault();
+//
+//     var number = parseInt($("#userNumberInput").val());
+//     var arrayNumbers = [];
+//
+//     for (var i = 0; i <= number - 1; i++) {
+//       arrayNumbers.push(i + 1);
+//
+//     }
